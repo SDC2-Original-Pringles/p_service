2f70c54134494274d8c50712c44a5877862dde5e
boltonlin <boltonlinbo@gmail.com>
2023-02-19 16:23:13 -0800

    removed log

--- service/controllers/index.js
+++ service/controllers/index.js
@@ -10,7 +10,6 @@ module.exports = {
   },
 
   getProductById(req, res) {
-    console.log(req);
     const { product_id } = req.params;
     if (!+product_id) return res.sendStatus(400);
     return model.readProductById(product_id)
