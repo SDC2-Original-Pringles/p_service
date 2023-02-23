a46d9cb327b8fd4e7e87b41e7ed127a80f5915a5
boltonlin <boltonlinbo@gmail.com>
2023-02-22 23:09:49 -0800

    removed unused files for db

--- routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const controllers = require('./controllers');
-
-router.get('/products', controllers.getProducts);
-router.get('/products/:product_id', controllers.getProductById);
-router.get('/products/:product_id/styles', controllers.getStylesByPid);
-router.get('/products/:product_id/related', controllers.getRelatedProducts);
-
-module.exports = router;
