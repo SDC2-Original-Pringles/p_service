3d1b562b353672f347b3ca52f02c252d3a36951e
boltonlin <boltonlinbo@gmail.com>
2023-02-23 01:12:58 -0800

    changed host

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
-const { Client } = require('cassandra-driver');
+const { Client } = require("cassandra-driver");
 
 const client = new Client({
-  contactPoints: [process.env.HOSTNAME],
+  contactPoints: [process.env.DB_HOST],
   localDataCenter: process.env.DB_DATACENTER,
   keyspace: process.env.DB_KEYSPACE,
 });
@@ -13,8 +13,8 @@ module.exports = {
       .then(({ rows: products }) =>
         products.slice(page * count - count).map((product) => ({
           ...product,
-          default_price: product.default_price.toString().concat('.00'),
-        }))
+          default_price: product.default_price.toString().concat(".00"),
+        })),
       );
   },
 
@@ -22,7 +22,7 @@ module.exports = {
     return Promise.all([
       client.execute(`SELECT * FROM products WHERE id=${id}`),
       client.execute(
-        `SELECT feature, value FROM features_by_product WHERE product_id=${id}`
+        `SELECT feature, value FROM features_by_product WHERE product_id=${id}`,
       ),
     ]).then(
       ([
@@ -32,9 +32,9 @@ module.exports = {
         { rows: features },
       ]) => ({
         ...product,
-        default_price: product.default_price.toString().concat('.00'),
+        default_price: product.default_price.toString().concat(".00"),
         features,
-      })
+      }),
     );
   },
 
@@ -43,7 +43,7 @@ module.exports = {
       .execute(`SELECT * FROM products_with_features WHERE id=${id}`)
       .then(({ rows: [product] }) => ({
         ...product,
-        default_price: product.default_price.toString().concat('.00'),
+        default_price: product.default_price.toString().concat(".00"),
       }));
   },
 
@@ -56,10 +56,10 @@ module.exports = {
             // eslint-disable-next-line prefer-const
             let [{ rows: photos }, { rows: skuRows }] = await Promise.all([
               client.execute(
-                `SELECT thumbnail_url, url FROM photos_by_style WHERE style_id=${style.id}`
+                `SELECT thumbnail_url, url FROM photos_by_style WHERE style_id=${style.id}`,
               ),
               client.execute(
-                `SELECT * FROM skus_by_style WHERE style_id=${style.id}`
+                `SELECT * FROM skus_by_style WHERE style_id=${style.id}`,
               ),
             ]);
             let skus = skuRows.reduce(
@@ -70,7 +70,7 @@ module.exports = {
                   size: skuRow.size,
                 },
               }),
-              {}
+              {},
             );
             if (!photos.length) photos = [{ thumbnail_url: null, url: null }];
             if (!Object.keys(skus).length)
@@ -80,19 +80,19 @@ module.exports = {
               name: style.name,
               original_price: style.original_price.toString(),
               sale_price: style.sale_price,
-              'default?': style.default_style,
+              "default?": style.default_style,
               photos,
               skus,
             };
-          })
-        )
+          }),
+        ),
       );
   },
 
   readRelatedProducts(current_pid) {
     return client
       .execute(
-        `SELECT related_pid FROM related_by_current WHERE current_pid=${current_pid}`
+        `SELECT related_pid FROM related_by_current WHERE current_pid=${current_pid}`,
       )
       .then(({ rows }) => rows.map((row) => row.related_pid));
   },
