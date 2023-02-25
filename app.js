4a6a2e737f074f53d613bd22e87eedd3abb9813f
boltonlin <boltonlinbo@gmail.com>
2023-02-25 00:16:35 -0800

    fix maybe

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-require("dotenv").config();
-const express = require("express");
-const morgan = require("morgan");
-const router = require("./routes");
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const router = require('./routes');
 
 const { HOSTNAME, PORT } = process.env;
 
 const app = express();
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(express.json());
 
-app.use("/loaderio-5ee06de27d37be9376fbd234d5191dfa", (req, res) =>
-  res.sendFile("./loaderio"),
+app.use('/loaderio-5ee06de27d37be9376fbd234d5191dfa', (req, res) =>
+  res.sendFile(path.join(__dirname, './loaderio')),
 );
-app.use("/", router);
+app.use('/', router);
 
 app.listen(PORT);
 // eslint-disable-next-line
