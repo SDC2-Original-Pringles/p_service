97f30d91ce4794ff279d6e04bc2b9966d960cd96
boltonlin <boltonlinbo@gmail.com>
2023-02-25 00:12:45 -0800

    add loaderio verification

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const router = require('./routes');
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const router = require("./routes");
 
 const { HOSTNAME, PORT } = process.env;
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
-// FILL YOUR ROUTE
-app.use('/', router);
+app.use("/loaderio-5ee06de27d37be9376fbd234d5191dfa", (req, res) =>
+  res.sendFile("./loaderio"),
+);
+app.use("/", router);
 
 app.listen(PORT);
 // eslint-disable-next-line
